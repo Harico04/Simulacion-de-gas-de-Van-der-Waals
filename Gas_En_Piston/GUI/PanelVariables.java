890372e782636b11cf824256f890611ffe0666d1
Mgb64 <a222210840@unison.mx>
2023-11-14 20:04:43 -0700

    Se empezo a trabajar en el Panel donde estarán los deslizadores, se iniciaron algunas variables en el código.

--- Gas_En_Piston/GUI/PanelVariables.java
+++ Gas_En_Piston/GUI/PanelVariables.java
@@ -2,11 +2,16 @@
  * JPanel que muestra los valores de las variables termodinamicas, permite 
  * su modificacion y que se fijen. Muestra creditos en la esquina inferior derecha.
  */
+
 package GUI;
 
 import javax.swing.JPanel;
 
-public class PanelVariables extends JPanel
-{
+public class PanelVariables extends JPanel{
+
+    private int temperatura;
+    private int presion;
+    private int moles;
+    private int volumen;
     
 }
