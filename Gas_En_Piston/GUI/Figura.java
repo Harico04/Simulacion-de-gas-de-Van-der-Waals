d5501d8a19d7582e4aa6efa5d11ea4d52ccfbcf5
Alan <alan_torres306@hotmail.com>
2023-11-19 20:11:37 -0700

    Agregada la funcion para modificar el volumen del piston y cambios en la interfaz figura

--- Gas_En_Piston/GUI/Figura.java
+++ Gas_En_Piston/GUI/Figura.java
@@ -2,7 +2,7 @@
  * Contiene lo esencial para que se realice un dibujo en el panel de dibujo.
  */
 package GUI;
-
+import java.awt.Graphics;
 public interface Figura
 {
 public void pintar(Graphics g);    
--- Gas_En_Piston/SistemaTermodinamico/Piston.java
+++ Gas_En_Piston/SistemaTermodinamico/Piston.java
@@ -8,7 +8,7 @@ package SistemaTermodinamico;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
-
+import java.awt.Graphics;
 import javax.swing.JPanel;
 import java.awt.BasicStroke;
 import java.awt.Rectangle;
@@ -16,16 +16,21 @@ public class Piston implements GUI.Figura
 
 {
   private final Color COLOR_CONTENEDOR = new Color(77, 71, 70), COLOR_PISTON = new Color(130,126,125);
-  private double volumen;
+  private double volumen,altura_piston;
   Point c1,c2,p1,p2;
-public Piston(Point c1, Point c2,double volumen ){ 
+  
+  public Piston(Point c1, Point c2,double volumen ){ 
     this.c1 = c1;
     this.c2 = c2;
-    double altura_piston = this.volumen/(Math.PI)*(200*200);
-    Rectangle piston = new Rectangle ((int)c1.getX(), (int) altura_piston, grosor, (int) (altura_piston-20));
+    altura_piston = this.volumen/(Math.PI)*(200*200);
+    Rectangle piston = new Rectangle ((int)c1.getX(), (int) altura_piston, (int)(c2.getX()-c1.getX()), (int) (altura_piston-20));
     Rectangle pI = new Rectangle ((int) c1.getX(),(int) c1.getY(), 1, (int) (c2.getY() - c1.getY()));
     Rectangle pD = new Rectangle ((int) c2.getX(),(int) c1.getY(), 1, (int) (c2.getY() - c1.getY()));
-    Rectangle pA = new Rectangle((int) c1.getX(), (int)c2.getY(),(int)(c2.getX()-c1.getX()) , 1)
+    Rectangle pA = new Rectangle((int) c1.getX(), (int)c2.getY(),(int)(c2.getX()-c1.getX()) , 1);
+  }
+  public void setVolumen(double volumen){
+    this.volumen = volumen;
+    altura_piston = this.volumen/(Math.PI)*(200*200);
   }
 
 public void pintar(Graphics g){
